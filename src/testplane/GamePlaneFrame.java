c068eb3b2462979c3e78b26b7c08e0739e7599f2
jiansuyan <test@SW-CNKC-SDR505L.apac.wdpr.disney.com>
2016-08-27 16:12:59 +0800

    plane27aug20160412

--- src/testplane/GamePlaneFrame.java
+++ src/testplane/GamePlaneFrame.java
@@ -2,19 +2,57 @@ package testplane;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 import util.*;
 
 public class GamePlaneFrame extends MyFrame {
 	Image bg = GameUtil.getImage("images/bg_01.jpg");
 	Plane p = new Plane("images/plane_01.jpg", 50, 50);
+	
+	ArrayList bulletlist = new ArrayList(); //泛型
 			
 	public void paint(Graphics g){
 		g.drawImage(bg, 0, 0, null);
 		p.draw(g);
+		for(int i = 0; i < bulletlist.size(); i++){
+			Bullet b = (Bullet) bulletlist.get(i);
+			b.draw(g);
+		}
 	}
 	
 	public static void main(String[] args){
 		new GamePlaneFrame().launchFrame();
+	}
+	
+	public void launchFrame(){
+		super.launchFrame();
+		//增加键盘的监听
+		addKeyListener(new KeyMonitor());
+		
+		//生成子弹
+		for(int i = 0; i <=50; i++){
+			Bullet b = new Bullet();
+			bulletlist.add(b);
+		}
+	}
+	
+	
+	// 定义为内部类，可以方便的使用外部类的普通属性
+	class KeyMonitor extends KeyAdapter{
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			System.out.println("Press: " + e.getKeyCode());
+			p.addDirection(e);
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			System.out.println("Release: " + e.getKeyCode());
+			p.minusDirection(e);
+		}
 	}
 }
--- src/testplane/Plane.java
+++ src/testplane/Plane.java
@@ -2,6 +2,7 @@ package testplane;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.KeyEvent;
 
 import util.*;
 
@@ -10,8 +11,65 @@ public class Plane {
 	Image img;
 	double x,y;
 	
+	boolean left, right, up, down;
+	int speed = 10;
+	
 	public void draw(Graphics g){
-		g.drawImage(img, 50, 50, null);
+		g.drawImage(img, (int)x, (int)y, null);
+		move();
+	}
+	
+	public void move(){
+		if(left){
+			x -= speed;
+		}
+		if(right){
+			x +=speed;
+		}
+		if(up){
+			y -= speed;
+		}
+		if(down){
+			y += speed;
+		}
+	}
+	
+	public void addDirection(KeyEvent e){
+		switch(e.getKeyCode()){
+		case KeyEvent.VK_LEFT:
+			left = true;
+			break;
+		case KeyEvent.VK_RIGHT:
+			right = true;
+			break;
+		case KeyEvent.VK_UP:
+			up = true;
+			break;
+		case KeyEvent.VK_DOWN:
+			down = true;
+			break;
+		default:
+			break;
+		}
+	}
+	
+	public void minusDirection(KeyEvent e){
+		switch(e.getKeyCode()){
+		case KeyEvent.VK_LEFT:
+			left = false;
+			break;
+		case KeyEvent.VK_RIGHT:
+			right = false;
+			break;
+		case KeyEvent.VK_UP:
+			up = false;
+			break;
+		case KeyEvent.VK_DOWN:
+			down = false;
+			break;
+		default:
+			break;
+		}
 	}
 	
 	public Plane(String imgpath, double x, double y) {
